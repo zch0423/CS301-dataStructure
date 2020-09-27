30abb15e1f8d264ad508d089176a8671a552b53a
zch0423 <zch0423@gmail.com>
2020-09-27 21:18:00 +0800

    顺序表实现

--- linearList.cpp
+++ linearList.cpp
@@ -2,4 +2,99 @@
 filename: linearList.cpp
 content: implementation of linear list structure defined in linearList.h
 */
+#include <iostream>
 #include "linearList.h"
+using namespace std;
+
+// implementation of seqList
+template<class elemType>
+seqList<elemType>::~seqList(){
+    delete []data;
+}
+
+template <class elemType>
+void seqList<elemType>::clear() {
+    currentLength = 0;
+}
+
+template <class elemType>
+int seqList<elemType>::length() const {
+    return currentLength;
+}
+
+template <class elemType>
+elemType seqList<elemType>::visit(int i) const {
+    return data[i];
+}
+
+template <class elemType>
+void seqList<elemType>::traverse() const {
+    cout<<endl;
+    for(int i=0;i<currentLength;++i){
+        cout<<data[i];
+    }
+}
+
+template <class elemType>
+seqList<elemType>::seqList(int initSize) {
+    data = new elemType[initSize];
+    maxSize = initSize;
+    currentLength = 0;
+}
+
+template <class elemType>
+int seqList<elemType>::search(const elemType &x) const {
+    for(int i=0;i<currentLength;++i){
+        if(data[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+
+template <class elemType>
+void seqList<elemType>::doubleSpace() {
+    elemType* temp = data;
+    maxSize *= 2;
+    data = new elemType[maxSize];
+    for(int i=0;i<currentLength;++i){
+        data[i] = temp[i];
+    }
+    delete []temp;
+}
+
+template<class elemType>
+void seqList<elemType>::insert(int i, const elemType &x) {
+    if(currentLength==maxSize){
+        doubleSpace();
+    }
+    for(int j=currentLength;j>i;--j){
+        data[j] = data[j-1];
+    }
+    data[i] = x;
+    ++currentLength;
+}
+
+template<class elemType>
+void seqList<elemType>::remove(int i) {
+    for(int j=i;j<currentLength-1;++j){
+        data[j] = data[j+1];
+    }
+    --currentLength;
+}
+
+template <class elemType>
+void seqList<elemType>::erase(int i) {
+    elemType temp = data[i];
+    int count = 0;
+    for(int j=0;j<currentLength;++j){
+        if(data[j]==temp){
+            ++count;
+        }else {
+            data[j - count] = data[j];
+        }
+    }
+    currentLength -= count;
+}
+
+// 单链表类
--- linearList.h
+++ linearList.h
@@ -1,6 +1,6 @@
 /*
 filename: linearList.h
-content: declaration of Linear List structure including seqlist linklist
+content: declaration of Linear List structure including seqList linklist
 */
 #ifndef _LIST_H
 #define _LIST_H
@@ -15,11 +15,12 @@ class list{
     virtual int search(const elemType& x)const=0;
     virtual elemType visit(int i)const=0;
     virtual void traverse()const =0;
-    virtual ~list(){};
+    virtual ~list()= default;
 };
 
 template<class elemType>
 class seqList:public list<elemType>{
+    // 顺序表
     private:
         elemType* data;
         int currentLength;
@@ -35,6 +36,7 @@ class seqList:public list<elemType>{
         void remove(int i);
         int search(const elemType&x)const;
         elemType visit(int i)const;
+        void erase(int i); // 删除所有等于第i个元素的元素 包括i
         void traverse()const;
 };
 
