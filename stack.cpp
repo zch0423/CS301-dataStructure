5484f96e8afb6d1276b211ee9ef7b1d70cd9a7b4
zch0423 <zch0423@gmail.com>
2020-10-13 22:41:50 +0800

    seqstack linkstack

--- stack.cpp
+++ stack.cpp
@@ -6,3 +6,90 @@
 #include "stack.h"
 #include <iostream>
 using namespace std;
+
+//stack 顺序栈
+template <typename T>
+seqStack<T>::seqStack(int initSize) {
+    data = new T[initSize];
+    topP = -1;
+    maxSize = initSize;
+}
+
+template <typename T>
+seqStack<T>::~seqStack() {
+    delete []data;
+}
+
+template <typename T>
+void seqStack<T>::doubleSpace() {
+    T* temp = data;
+    maxSize *= 2;
+    data = new T[maxSize];
+    for (int i = 0; i < maxSize; ++i) {
+        data[i] = temp[i];
+    }
+    delete []temp;
+}
+
+template <typename T>
+bool seqStack<T>::isEmpty() const {
+    return topP==-1;
+}
+
+template <typename T>
+void seqStack<T>::push(const T &temp) {
+    if(topP==maxSize-1){
+        doubleSpace();
+    }
+    data[++topP] = temp;
+}
+
+template <typename T>
+T seqStack<T>::pop() {
+    return data[topP--];
+}
+
+template <typename T>
+T seqStack<T>::top() const {
+    return data[topP];
+}
+
+//链表栈
+template <typename T>
+linkStack<T>::linkStack() {
+    topP= nullptr;
+}
+
+template <typename T>
+linkStack<T>::~linkStack() {
+    node* temp;
+    while(topP!= nullptr){
+        temp = topP->next;
+        delete topP;
+        topP = temp;
+    }
+}
+
+template <typename T>
+bool linkStack<T>::isEmpty() const {
+    return topP== nullptr;
+}
+
+template <typename T>
+void linkStack<T>::push(const T &temp) {
+    topP = new node(temp, topP)
+}
+
+template <typename T>
+T linkStack<T>::pop() {
+    node* temp = topP;
+    T data = temp->data;
+    topP = topP->next;
+    delete temp;
+    return data;
+}
+
+template <typename T>
+T linkStack<T>::top() const {
+    return topP->data;
+}
--- stack.h
+++ stack.h
@@ -6,7 +6,53 @@ Content: declaration of stack structure
 
 #ifndef CLIONPROJECT_STACK_H
 #define CLIONPROJECT_STACK_H
+// stack 抽象类
+template <typename T>
+class stack{
+public:
+    virtual bool isEmpty()const=0;
+    virtual void push(const T& temp)=0;
+    virtual T pop()=0;
+    virtual T top()const =0;
+    virtual ~stack(){}
+};
 
+// 顺序栈
+template <typename T>
+class seqStack:public stack<T>{
+private:
+    T* data;
+    int maxSize;
+    T topP;
+    void doubleSpace();
+public:
+    explicit seqStack(int initSize=10);
+    bool isEmpty()const;
+    void push(const T& temp);
+    T pop();
+    T top()const;
+    ~seqStack();
+};
 
+// 链表栈
+template <typename T>
+class linkStack:public stack<T>{
+private:
+    struct node{
+        T data;
+        node* next;
+        node(const T&temp, node* n= nullptr){data=temp;next=n;}
+        node(){next= nullptr;}
+        ~node(){}
+    };
+    node* topP;
+public:
+    linkStack();
+    ~linkStack();
+    bool isEmpty()const;
+    void push(const T&temp);
+    T pop();
+    T top()const ;
+};
 
 #endif //CLIONPROJECT_STACK_H
